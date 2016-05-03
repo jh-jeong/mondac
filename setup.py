b2cfa527e2a37311d344f29f8827272e3aab2a2f
Jongheon Jeong <biscuit@kaist.ac.kr>
2016-05-03 15:12:23 +0900

    ADD license string in setup()

--- setup.py
+++ setup.py
@@ -5,9 +5,8 @@ setup(
     version='0.1.0',
     packages=['test', 'mondac', 'mondac.utils'],
     url='',
-    license='',
+    license='GNU General Public License v3.0',
     author='Jongheon Jeong',
     author_email='jongheonj@exbrain.io',
-    description='MONDrian Algorithm Configuration: '
-                'Module for algorithm configuration with Mondrian forest'
+    description='MONDrian Algorithm Configuration: Module for algorithm configuration with Mondrian forest'
 )
